5196ba81f2d6fb5f69feebe202df63f2dec1db9c
Nova <noah.trauben@gmail.com>
2018-11-10 22:08:26 -0500

    .

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src import serialSend
 from src import pgp
+from src import findPort
 
 cipher = pgp.encrypt("hello World", "testUser1")
-serialSend.send("/dev/tty0", cipher)
+serialSend.send(findPort.findScreen(), cipher)
 
